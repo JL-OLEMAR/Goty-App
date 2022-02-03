084d1be463fa2ae9ced0afe16a5d78632b7beb99
JL-OLEMAR <joseluis19963@gmail.com>
2022-02-02 23:50:34 -0500

    Componente Navbar

---
+++ src/app/pages/goty/goty.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-goty',
+  templateUrl: './goty.component.html',
+  styleUrls: ['./goty.component.css']
+})
+export class GotyComponent implements OnInit {
+  // constructor () { }
+
+  ngOnInit (): void {
+  }
+}
